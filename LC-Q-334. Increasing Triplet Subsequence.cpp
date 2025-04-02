8ca988970c211b84298d8c851b0add2eb5bbc2d4
Ansh dev nagar <anshdevnagar@gmail.com>
2025-04-02 17:37:31 +0530

    Update LC-Q-334. Increasing Triplet Subsequence.cpp

--- LC-Q-334. Increasing Triplet Subsequence.cpp
+++ LC-Q-334. Increasing Triplet Subsequence.cpp
@@ -60,3 +60,20 @@ public:
          return false;
     }
 };
+
+//SOLUTION-2- S(n) = O(1) 
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+     
+        int first=INT_MAX,second=INT_MAX;
+        for(int n:nums){
+            if(n<=first) first=n;
+            else if(n<=second) second=n;
+            else return true;
+        }
+         return false;
+    }
+};
+
